a5212a01abb3288b32c3971a7c453cb72aa9e430
François Michaud <fmichaud@users.noreply.github.com>
2022-01-30 22:52:14 +0100

    V0.0.10 - Fix invalid import (#11)
    * Upgrade version
    
    * Fix invalid import

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from distutils.core import Command
 __name__ = "wipbox"
 """ The project name. """
 
-__version__ = "0.0.9"
+__version__ = "0.0.10"
 """ The current version (work in progress). """
 
 
@@ -179,7 +179,7 @@ setup(
         "deepnox.aiorest": "src/deepnox/aiorest",
         "deepnox.auth": "src/deepnox/auth",
         "deepnox.clients": "src/deepnox/clients",
-        "deepnox.core": "src/deepnox/clients",
+        "deepnox.core": "src/deepnox/core",
         "deepnox.files": "src/deepnox/files",
         "deepnox.helpers": "src/deepnox/clients",
         "deepnox.loggers": "src/deepnox/loggers",
